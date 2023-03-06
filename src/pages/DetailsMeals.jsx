fa6444436a2ee4d5b5936fbdcb9ef48c1f3b9e7e
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-03-06 15:20:21 -0300

    Requisito 24

--- src/pages/DetailsMeals.jsx
+++ src/pages/DetailsMeals.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-export default function DetailsMeals() {
+export default function DetailsMeals({ match: { params: { id } } }) {
+  const [detailsMeals, setDetailsMeals] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+      .then((respose) => respose.json())
+      .then((data) => setDetailsMeals(data.meals));
+  }, [id]);
+
+  console.log(detailsMeals);
+
   return (
     <section>
       <h1>Details Meals</h1>
     </section>
   );
 }
+
+DetailsMeals.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};

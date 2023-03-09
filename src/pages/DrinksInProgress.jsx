bc084b31168762fdcc1a0d8e70714f9141f6d261
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-03-09 12:22:28 -0300

    lint erro

--- src/pages/DrinksInProgress.jsx
+++ src/pages/DrinksInProgress.jsx
@@ -65,12 +65,6 @@ export default function DrinksInProgress({ match: { params: { id } } }) {
 
     markedIngredient.current = result.map((item) => item.ingredient);
 
-    const {
-      strDrink,
-      strCategory,
-      strDrinkThumb,
-      strInstructions,
-    } = detailsRecipes[0];
     const ingredientsAndMeasures = ingredients.map((item, index) => ({
       ingredient: item,
       measure: measures[index],
@@ -134,40 +128,6 @@ export default function DrinksInProgress({ match: { params: { id } } }) {
                   onChange={ (event) => doneStep(event) }
                   checked={ inProgress.drinks[id]
                   && inProgress.drinks[id].includes(item.ingredient) }
-              />
-            </label>
-          </div>
-        ))}
-        <h3>Instructions</h3>
-        <p data-testid="instructions">{strInstructions}</p>
-      </div>
-    );
-  }
-
-  console.log(inProgress.drinks[id]);
-  const test = inProgress.drinks[id];
-  console.log(markedIngredient.current);
-  const disabled = markedIngredient.current.length !== 0
-    ? markedIngredient.current
-      .every((item, index) => item === test[index])
-    : false;
-  console.log(disabled);
-
-  return (
-    <section>
-      <h1>DrinksInProgress</h1>
-      { detailsRecipes && details() }
-      <button type="button" data-testid="share-btn">
-        Share
-      </button>
-      <button type="button" data-testid="favorite-btn">
-        Favorite
-      </button>
-      <button
-        type="button"
-        data-testid="finish-recipe-btn"
-        onClick={ handleClick }
-        disabled={ !disabled }
                 />
               </label>
             </div>
@@ -177,6 +137,14 @@ export default function DrinksInProgress({ match: { params: { id } } }) {
         </div>
       ) }
       <ButtonsFavoriteShare id={ id } type={ detailsRecipes } />
+      <button
+        type="button"
+        data-testid="finish-recipe-btn"
+        disabled={ isEnable.current }
+        onClick={ handleClick }
+      >
+        Finish Recipe
+      </button>
     </section>
   );
 }

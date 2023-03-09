6bddb4c1c1d79f8746b7730594c3bc90fe6761ba
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-03-09 14:59:42 -0300

    grupo-21 - req 57/62

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer/Index';
 import Header from '../components/Header';
 
 function Profile() {
+  const history = useHistory();
+
   return (
     <section>
       <Header title="Profile" isIconProfile />
+      <p data-testid="profile-email">
+        Email:
+        {' '}
+        {JSON.parse(localStorage.getItem('user')).email}
+      </p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+      >
+        Logout
+      </button>
       <Footer />
     </section>
   );

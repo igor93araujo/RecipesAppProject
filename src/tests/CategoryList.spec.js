a4547993caa036c957a665b8d828bad30c1ec9bc
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-03-11 02:14:03 -0300

    grupo-21 - erro lint

--- src/tests/CategoryList.spec.js
+++ src/tests/CategoryList.spec.js
@@ -1,4 +1,4 @@
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import App from '../App';

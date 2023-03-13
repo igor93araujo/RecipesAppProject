d2d21cb0c655db5c4271c742a796bb17e40c4e80
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-03-13 16:24:39 -0300

    Estilizando a página de profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer/Index';
 import Header from '../components/Header';
+import allImg from '../images/All.png';
+import allFavorit from '../images/AllFavorit.png';
+import allLogout from '../images/allLogout.png';
+import './style/Profile.css';
 
 function Profile() {
   const history = useHistory();
@@ -10,33 +14,40 @@ function Profile() {
   return (
     <section>
       <Header title="Profile" isIconProfile />
-      <p data-testid="profile-email">
-        {`Email: ${email.email}`}
-      </p>
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-        onClick={ () => history.push('/done-recipes') }
-      >
-        Done Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-        onClick={ () => history.push('/favorite-recipes') }
-      >
-        Favorite Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ () => {
-          localStorage.clear();
-          history.push('/');
-        } }
-      >
-        Logout
-      </button>
+      <main className="main-profile">
+        <p data-testid="profile-email">
+          {`Email: ${email.email}`}
+        </p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          <img src={ allImg } alt="img" className="img-profile" />
+          Done Recipes
+        </button>
+        <hr />
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          <img src={ allFavorit } alt="favorit" className="img-profile" />
+          Favorite Recipes
+        </button>
+        <hr />
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => {
+            localStorage.clear();
+            history.push('/');
+          } }
+        >
+          <img src={ allLogout } alt="logout" className="img-profile" />
+          Logout
+        </button>
+      </main>
       <Footer />
     </section>
   );

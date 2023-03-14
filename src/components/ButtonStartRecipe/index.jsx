c254e764c8307f7d09acb1fb1a8f3394aa2989ef
Igor <igoraraujobda@gmail.com>
2023-03-14 00:29:39 -0300

    grupo21-correcao do css e criação da logica de alternar entre começar e continuar uma receita

--- src/components/ButtonStartRecipe/index.jsx
+++ src/components/ButtonStartRecipe/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './ButtonStartRecipe.css';
 
 export default function ButtonStartRecipe({ url, id }) {
   const history = useHistory();
+  const [recipeStarted, setRecipeStarted] = useState(false);
+
+  useEffect(() => {
+    const recipe = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    if (!recipe) return;
+
+    const isRecipeStarted = url === 'drinks' ? recipe.cocktails[id] : recipe.meals[id];
+    if (isRecipeStarted) {
+      setRecipeStarted(true);
+    }
+  }, [id, url]);
+
   const handleClick = () => {
     if (url === 'drinks') {
       history.push(`/drinks/${id}/in-progress`);
@@ -18,7 +30,7 @@ export default function ButtonStartRecipe({ url, id }) {
       data-testid="start-recipe-btn"
       onClick={ () => handleClick() }
     >
-      Continue Recipe
+      {recipeStarted ? 'Continue recipe' : 'Start recipe'}
     </button>
   );
 }
